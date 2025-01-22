1b656e458d8d0cf1441944bc4fe6f4a92b955b5d
Kushagra Singh <iamkushagra2021@gmail.com>
2025-01-23 00:46:01 +0530

    second commit

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 const path=require('path');
+const cookieParser=require('cookie-parser');
+
 const userRoute = require('./routes/user');
+const { checkForAuthenticationCookie } = require('./middleware/authentication');
 const PORT=3000;
 
 
@@ -11,10 +14,15 @@ app.set('view engine','ejs');
 app.set('views',path.resolve("./views"));
 app.use(express.urlencoded({ extended:false}));
 
+app.use(cookieParser());
+app.use(checkForAuthenticationCookie("token"));
+
 mongoose.connect('mongodb+srv://fake2113032:king123@cluster0.aldvw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(e=>console.log('MongoDB Connected'))
 
 app.get('/',(req,res)=>{
-    res.render("home");
+    res.render("home",{
+        user:req.user,
+    });
 });
 
 app.use('/user',userRoute);
--- routes/user.js
+++ routes/user.js
@@ -12,12 +12,22 @@ router.get('/signup',(req, res)=>{
 
 router.post('/signin',async(req, res)=>{
     const{email,password}=req.body;
-   const user= await User.matchPassword(email,password)
-
-console.log("User",user);
-    return res.redirect('/');
-
+    try{
+        
+   const token= await User.matchPasswordAndGenerateToken(email,password);
+    return res.cookie("token",token).redirect('/');
+    }
+    catch(error){
+         return res.render("signin",{
+            error:"Invalid email or password"
+         });
+    }
 });
+
+router.get('/logout', (req, res)=>{
+    res.clearCookie("token");
+    return res.redirect('/');
+})
 
 router.post('/signup',async(req, res)=>{
     const{fullName,email,password}=req.body;
